c2844cb5a6adde1b6699721cb4473d260885fa43
Muhammadgi <khanhkharis1@gmail.com>
2026-05-15 10:36:05 +0500

    added to all files

---
+++ Day1/sum_fun.py
@@ -0,0 +1,7 @@
+def sum(num1,num2):
+    sum=num1+num2
+    return sum
+
+results=sum(12,12)
+
+print(results)
---
+++ Day1/table.py
@@ -0,0 +1,8 @@
+def table(number):
+    
+    for x in range(1,11):
+        results=number*x
+        print(f'{number} x {x} = {results}' )
+    print()
+
+table(2)
